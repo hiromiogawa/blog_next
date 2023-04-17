387616420a64cc8cd34dad3238ab1e2cf58f3c80
hiromi <hiromi.ogawa.1223@gmail.com>
2023-04-17 21:17:36 +0900

    update: side Tag追加

--- src/components/organisms/SideContents.tsx
+++ src/components/organisms/SideContents.tsx
@@ -19,7 +19,7 @@ const SideContents = ({ categories, tags }: SideContentsType) => {
     <StyledAside>
       <section>
         <StyledHeading1>CATEGORIES</StyledHeading1>
-        <StyledUl>
+        <StyledCategories>
           {categories.map((category) => (
             <li key={category.name}>
               <StyledLink href={`/blog/${category.id}/1`}>
@@ -34,19 +34,19 @@ const SideContents = ({ categories, tags }: SideContentsType) => {
               </StyledLink>
             </li>
           ))}
-        </StyledUl>
+        </StyledCategories>
       </section>
-      <section>
+      <StyledTagsSection>
         <StyledHeading1>TAGS</StyledHeading1>
-        <ul>
+        <StyledTags>
           {tags.length > 0 &&
             tags.map((tag) => (
               <li key={tag.name}>
-                <TagText id={tag.id}>{tag.name}</TagText>
+                <StyledTagText id={tag.id}>{tag.name}</StyledTagText>
               </li>
             ))}
-        </ul>
-      </section>
+        </StyledTags>
+      </StyledTagsSection>
     </StyledAside>
   )
 }
@@ -63,7 +63,7 @@ const StyledHeading1 = styled(Heading1)`
   font-size: 20px;
 `
 
-const StyledUl = styled.ul`
+const StyledCategories = styled.ul`
   display: flex;
   gap: 13px;
   flex-wrap: wrap;
@@ -78,10 +78,10 @@ const StyledLink = styled(Link)`
   background: #fff;
   border-radius: 12px;
   border: solid 1px #00ae95;
-  box-shadow: -5px 5px 0px 0px rgba(0, 174, 149, 1);
+  box-shadow: -3px 3px 0px 0px rgba(0, 174, 149, 1);
 
   &:hover {
-    transform: translate(-5px, 5px);
+    transform: translate(-3px, 3px);
     box-shadow: 0px 0px 0px 0px rgba(0, 174, 149, 1);
   }
 `
@@ -97,4 +97,30 @@ const StyledImageWrap = styled.div`
 const StyledImage = styled(Image)`
   width: 100%;
   height: auto;
+`
+
+const StyledTagsSection = styled.section`
+  margin-top: 40px;
+`
+
+const StyledTags = styled.ul`
+  margin-top: 16px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 6px;
+`
+
+const StyledTagText = styled(TagText)`
+  display: inline-block;
+  font-size: 12px;
+  padding: 8px;
+  background-color: #fff;
+  border-radius: 12px;
+  border: solid 1px #00ae95;
+  box-shadow: -3px 3px 0px 0px rgba(0, 174, 149, 1);
+
+  &:hover {
+    transform: translate(-3px, 3px);
+    box-shadow: 0px 0px 0px 0px rgba(0, 174, 149, 1);
+  }
 `
