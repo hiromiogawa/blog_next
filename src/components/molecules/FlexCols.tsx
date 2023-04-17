7819e38d8f3b93d240917a027f21cc33114714d7
hiromi <hiromi.ogawa.1223@gmail.com>
2023-04-17 21:44:02 +0900

    add: FlexCols

---
+++ src/components/molecules/FlexCols.tsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+export type FlexColsType = {
+  tag?: React.ElementType
+  col: number
+  gap: number
+  children: React.ReactDOM
+}
+
+const FlexCols = ({
+  tag = 'ul',
+  col,
+  gap,
+  children,
+  ...props
+}: FlexColsType) => {
+  return (
+    <StyledFlexCols as={tag} col={col} gap={gap} {...props}>
+      {children}
+    </StyledFlexCols>
+  )
+}
+
+export default FlexCols
+
+const StyledFlexCols = styled.ul<Pick<FlexColsType, 'col' | 'gap'>>`
+  display: flex;
+  gap: ${({ gap }) => `${gap}px`};
+
+  > * {
+    width: ${({ gap, col }) =>
+      `calc((100% - (${gap}px * ${col - 1})) / ${col})`};
+  }
+`
