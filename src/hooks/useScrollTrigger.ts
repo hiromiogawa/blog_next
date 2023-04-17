6cd300d80db7fb170f8a694d546e832b1a2d3b32
hiromi <hiromi.ogawa.1223@gmail.com>
2023-04-17 22:57:38 +0900

    update: useScrollTrigger コメント変更

--- src/hooks/useScrollTrigger.ts
+++ src/hooks/useScrollTrigger.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState, RefObject } from 'react'
 
 /**
- * 指定されたHTML要素が表示領域内にスクロールされたかどうかを監視するカスタムReactフックです。
+ * 指定されたHTML要素が表示領域内にスクロールされたかどうかを監視するカスタムhook
  */
 export const useScrollTrigger = (
   ref: RefObject<HTMLElement>,
