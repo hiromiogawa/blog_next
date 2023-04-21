9d2f36087eeb7a70f5551dda92ff342c164e0d63
hiromi <hiromi.ogawa.1223@gmail.com>
2023-04-21 09:48:17 +0900

    add: DetailContent

---
+++ src/components/organisms/DetailContent.tsx
@@ -0,0 +1,190 @@
+import styled from 'styled-components'
+import { Device } from '@/styles/vars'
+import mediaQuery from '@/styles/functions/mediaQuery'
+
+const DetailContent = styled.div`
+  ${mediaQuery(Device.tb)} {
+    margin-top: 32px;
+  }
+  > * {
+    line-height: 1.6;
+
+    &:first-child {
+      margin-top: 0 !important;
+    }
+
+    &:not(:first-child) {
+      margin-top: 16px;
+    }
+  }
+
+  /* 見出し */
+  h1,
+  h2,
+  h3,
+  h4,
+  h5 {
+    line-height: 1.4;
+  }
+
+  h1 {
+    margin-top: 64px;
+    font-size: 32px;
+    background: #fff;
+    padding: 10px;
+    color: #00ae95;
+    border-radius: 12px;
+    border: solid 3px #00ae95;
+
+    ${mediaQuery(Device.tb)} {
+      font-size: 28px;
+      padding: 6px;
+    }
+
+    & + * {
+      margin-top: 24px;
+    }
+  }
+
+  h2 {
+    margin-top: 48px;
+    font-size: 28px;
+    border-bottom: double 5px #00ae95;
+    ${mediaQuery(Device.tb)} {
+      font-size: 24px;
+    }
+
+    & + * {
+      margin-top: 20px;
+    }
+  }
+
+  h3 {
+    margin-top: 32px;
+    font-size: 24px;
+    padding: 4px 4px;
+    color: #00ae95;
+    background: transparent;
+    border-left: solid 5px #00ae95;
+    ${mediaQuery(Device.tb)} {
+      font-size: 20px;
+      padding: 2px 2px;
+    }
+  }
+
+  h4 {
+    margin-top: 24px;
+    font-size: 20px;
+    border-bottom: solid 3px #00ae95;
+    ${mediaQuery(Device.tb)} {
+      font-size: 18px;
+    }
+  }
+
+  h5 {
+    margin-top: 20px;
+    font-size: 18px;
+    color: #00ae95;
+    ${mediaQuery(Device.tb)} {
+      font-size: 16px;
+    }
+  }
+
+  /* 段落 */
+  p {
+    font-size: 1rem;
+  }
+
+  p span {
+    display: inline-block;
+  }
+
+  /* リスト */
+  ol,
+  ul {
+    padding-left: 2rem;
+
+    li {
+      &:not(:first-child) {
+        margin-top: 0.8em;
+      }
+    }
+  }
+
+  ol {
+    list-style: decimal;
+  }
+
+  ul {
+    list-style: outside;
+  }
+
+  em {
+    font-style: italic;
+  }
+
+  /* 引用 */
+  blockquote {
+    font-style: italic;
+    background-color: #f1f1f1;
+    border-left: 5px solid #ccc;
+    padding: 1rem;
+  }
+
+  /* コード */
+
+  code {
+    font-family: 'Source Code Pro', monospace;
+    font-size: 14px;
+    color: #660066;
+  }
+
+  pre {
+    line-height: 1.4;
+    background-color: #000;
+    color: #fff;
+    border-radius: 4px;
+    padding: 1rem;
+    overflow-x: auto;
+
+    code {
+      color: #fff;
+    }
+  }
+
+  /* リンク */
+  a {
+    color: #00ae95;
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  /* 画像 */
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+    margin: 1rem auto;
+  }
+
+  sub,
+  sup {
+    font-size: 0.75em;
+    line-height: 0;
+    position: relative;
+    vertical-align: baseline;
+  }
+
+  sub {
+    bottom: -0.25em;
+  }
+
+  sup {
+    top: -0.5em;
+  }
+`
+
+export default DetailContent
--- src/pages/blog/detail/[id].tsx
+++ src/pages/blog/detail/[id].tsx
@@ -2,24 +2,17 @@ import styled from 'styled-components'
 import { MAX_LIMIT } from '@/config'
 import { Device } from '@/styles/vars'
 
-// hooks
-import useMediaQuery from '@/hooks/useMediaQuery'
-
 // functions
 import { getBlog, getBlogs, getCategories, getTags } from '@/functions/getData'
 import mediaQuery from '@/styles/functions/mediaQuery'
-import getCardListData from '@/functions/getCardListData'
 
 // components
 import Layout, { LayoutType } from '@/components/common/Layout'
 import ContentsHead from '@/components/common/ContentsHead'
-import DayTime1 from '@/components/atoms/text/DayTime1'
-import TagList from '@/components/molecules/TagList'
-import CategoryText from '@/components/atoms/text/CategoryText'
+import DetailMeta from '@/components/organisms/DetailMeta'
+import DetailContent from '@/components/organisms/DetailContent'
 import Html from '@/components/elements/Html'
-import Heading1 from '@/components/atoms/text/Heading1'
-import SlideCardList from '@/components/organisms/SlideCardList'
-import VerticalCardList from '@/components/organisms/VerticalCardList'
+import DetailRelated from '@/components/organisms/DetailRelated'
 
 // types
 import type { GetStaticProps } from 'next'
@@ -36,45 +29,16 @@ export type PropTypes = {
 } & LayoutType
 
 const Detail = ({ blog, categories, tags }: PropTypes) => {
-  const isTb = useMediaQuery('tb')
-  const cardListData = getCardListData(blog.connections)
-
   return (
     <Layout categories={categories} tags={tags}>
       <ContentsHead title={`${blog.title} | `} />
       <StyledDetail>
-        <StyledMeta>
-          <StyledCategoryText size="12px" id={blog.category.id}>
-            {blog.category.name}
-          </StyledCategoryText>
-          <StyledDay>
-            <li>
-              投稿日: <StyledDayTime1>{blog.createdAt}</StyledDayTime1>
-            </li>
-            <li>
-              更新日: <StyledDayTime1>{blog.updatedAt}</StyledDayTime1>
-            </li>
-          </StyledDay>
-          <StyledTitle>{blog.title}</StyledTitle>
-          <StyledTagList tags={blog.tags} />
-        </StyledMeta>
-
-        <StyledContent>
+        <DetailMeta {...blog} />
+        <StyledDetailContent>
           <Html>{blog.content}</Html>
-        </StyledContent>
+        </StyledDetailContent>
       </StyledDetail>
-
-      <StyledRelated>
-        <StyledHeading1>Related article</StyledHeading1>
-        {isTb ? (
-          <StyledVerticalCardList
-            showFooter={false}
-            cardListData={cardListData}
-          />
-        ) : (
-          <StyledSlideCardList showFooter={false} cardListData={cardListData} />
-        )}
-      </StyledRelated>
+      <StyledDetailRelated blogs={blog.connections} />
     </Layout>
   )
 }
@@ -93,274 +57,12 @@ const StyledDetail = styled.section`
   }
 `
 
-const StyledTitle = styled.h1`
-  margin-top: 8px;
-  font-size: 32px;
-  background: #00ae95;
-  color: #fff;
-  padding: 10px;
-  border-radius: 12px;
-  line-height: 1.4;
-
-  ${mediaQuery(Device.tb)} {
-    font-size: 28px;
-    padding: 6px;
-  }
-`
-
-const StyledMeta = styled.div`
-  font-size: 16px;
-`
-
-const StyledDay = styled.ul`
-  font-size: 16px;
-  display: flex;
-  justify-content: flex-end;
-  margin-top: -1em;
-  gap: 8px;
-  ${mediaQuery(Device.tb)} {
-    justify-content: flex-start;
-    margin-top: 4px;
-    font-size: 14px;
-  }
-  ${mediaQuery(Device.sp)} {
-    display: block;
-
-    > *:not(:first-child) {
-      margin-top: 4px;
-    }
-  }
-`
-
-const StyledCategoryText = styled(CategoryText)`
-  font-size: 16px;
-
-  a {
-    color: #00ae95;
-
-    &:hover {
-      text-decoration: underline;
-    }
-  }
-`
-
-const StyledDayTime1 = styled(DayTime1)`
-  font-size: 16px;
-`
-
-const StyledTagList = styled(TagList)`
-  margin-top: 4px;
-  font-size: 14x;
-
-  a {
-    color: #00ae95;
-
-    &:hover {
-      text-decoration: underline;
-    }
-  }
-`
-
-const StyledContent = styled.div`
+const StyledDetailContent = styled(DetailContent)`
   margin-top: 40px;
-
-  ${mediaQuery(Device.tb)} {
-    margin-top: 32px;
-  }
-  > * {
-    line-height: 1.6;
-
-    &:first-child {
-      margin-top: 0 !important;
-    }
-
-    &:not(:first-child) {
-      margin-top: 16px;
-    }
-  }
-
-  /* 見出し */
-  h1,
-  h2,
-  h3,
-  h4,
-  h5 {
-    line-height: 1.4;
-  }
-
-  h1 {
-    margin-top: 64px;
-    font-size: 32px;
-    background: #fff;
-    padding: 10px;
-    color: #00ae95;
-    border-radius: 12px;
-    border: solid 3px #00ae95;
-
-    ${mediaQuery(Device.tb)} {
-      font-size: 28px;
-      padding: 6px;
-    }
-
-    & + * {
-      margin-top: 24px;
-    }
-  }
-
-  h2 {
-    margin-top: 48px;
-    font-size: 28px;
-    border-bottom: double 5px #00ae95;
-    ${mediaQuery(Device.tb)} {
-      font-size: 24px;
-    }
-
-    & + * {
-      margin-top: 20px;
-    }
-  }
-
-  h3 {
-    margin-top: 32px;
-    font-size: 24px;
-    padding: 4px 4px;
-    color: #00ae95;
-    background: transparent;
-    border-left: solid 5px #00ae95;
-    ${mediaQuery(Device.tb)} {
-      font-size: 20px;
-      padding: 2px 2px;
-    }
-  }
-
-  h4 {
-    margin-top: 24px;
-    font-size: 20px;
-    border-bottom: solid 3px #00ae95;
-    ${mediaQuery(Device.tb)} {
-      font-size: 18px;
-    }
-  }
-
-  h5 {
-    margin-top: 20px;
-    font-size: 18px;
-    color: #00ae95;
-    ${mediaQuery(Device.tb)} {
-      font-size: 16px;
-    }
-  }
-
-  /* 段落 */
-  p {
-    font-size: 1rem;
-  }
-
-  p span {
-    display: inline-block;
-  }
-
-  /* リスト */
-  ol,
-  ul {
-    padding-left: 2rem;
-
-    li {
-      &:not(:first-child) {
-        margin-top: 0.8em;
-      }
-    }
-  }
-
-  ol {
-    list-style: decimal;
-  }
-
-  ul {
-    list-style: outside;
-  }
-
-  em {
-    font-style: italic;
-  }
-
-  /* 引用 */
-  blockquote {
-    font-style: italic;
-    background-color: #f1f1f1;
-    border-left: 5px solid #ccc;
-    padding: 1rem;
-  }
-
-  /* コード */
-
-  code {
-    font-family: 'Source Code Pro', monospace;
-    font-size: 14px;
-    color: #660066;
-  }
-
-  pre {
-    line-height: 1.4;
-    background-color: #000;
-    color: #fff;
-    border-radius: 4px;
-    padding: 1rem;
-    overflow-x: auto;
-
-    code {
-      color: #fff;
-    }
-  }
-
-  /* リンク */
-  a {
-    color: #00ae95;
-    text-decoration: none;
-  }
-
-  a:hover {
-    text-decoration: underline;
-  }
-
-  /* 画像 */
-  img {
-    max-width: 100%;
-    height: auto;
-    display: block;
-    margin: 1rem auto;
-  }
-
-  sub,
-  sup {
-    font-size: 0.75em;
-    line-height: 0;
-    position: relative;
-    vertical-align: baseline;
-  }
-
-  sub {
-    bottom: -0.25em;
-  }
-
-  sup {
-    top: -0.5em;
-  }
 `
 
-const StyledRelated = styled.section`
+const StyledDetailRelated = styled(DetailRelated)`
   margin-top: 80px;
-`
-
-const StyledHeading1 = styled(Heading1)`
-  font-size: 20px;
-`
-
-const StyledVerticalCardList = styled(VerticalCardList)`
-  margin-top: 24px;
-`
-const StyledSlideCardList = styled(SlideCardList)`
-  margin-top: 24px;
 `
 
 // 動的なページを作成
