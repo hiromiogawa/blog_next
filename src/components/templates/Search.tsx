8e6b51fcea22726009bbca159e0eb801d03aceef
hiromi <hiromi.ogawa.1223@gmail.com>
2023-04-22 09:52:06 +0900

    update: 検索戻るボタン修正

--- src/components/templates/Search.tsx
+++ src/components/templates/Search.tsx
@@ -57,7 +57,7 @@ const Search = () => {
       searchblogs(keyword, page)
       setIsLoading(true)
     }
-  }, [keyword, isLoading])
+  }, [keyword])
 
   useEffect(() => {
     setCardListData(getCardListData(blogs))
